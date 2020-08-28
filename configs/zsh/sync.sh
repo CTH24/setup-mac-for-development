92ce1ed3dd8847bafc4eacd7f0c3943d556b37bf
Daniel Hoffmann <daniel.hoffmann@cth24.de>
2020-08-28 23:39:16 +0200

    dotfiles

--- configs/zsh/sync.sh
+++ configs/zsh/sync.sh
@@ -1,11 +1,12 @@
 c_sync_configs() {
-  c_title "Sync Configuration"
+
   cd ~/code/setup-mac-for-development
   sh ./sync-dotfiles.sh
   g dotfiles
   cd ~/code/python-scripts
   g
   clear
+  c_title "Sync Configuration"
   c_fortune
 }
 alias engage=c_sync_configs
