8ea6c6eacd7c654133ca968b17b78cad3f3dd9a1
Daniel Hoffmann <daniel.hoffmann@cth24.de>
2020-08-28 23:31:11 +0200

    dotfiles

--- configs/zsh/shortcuts.sh
+++ configs/zsh/shortcuts.sh
@@ -3,3 +3,19 @@ alias t="touch"
 alias c="code ."
 alias v="vim"
 alias web="open https://google.com"
+
+# Find Text in Folder
+c_ft() {
+  c_title "Find Text in Folder"
+  grep -rnw '.' -e "$1"
+  c_fortune
+}
+alias ft=c_ft
+
+# Fint File in Folder
+c_ff() {
+  c_title "Find File in Folder"
+  find . -name "$1" -type f
+  c_fortune
+}
+alias ff=c_ff
