9d03f2161f0a61f662db4bf32c033bf2bbfb741b
Daniel Hoffmann <daniel.hoffmann@cth24.de>
2020-09-10 16:44:06 +0200

    powerlevel9k

--- setup/basics/ohmyzsh.sh
+++ setup/basics/ohmyzsh.sh
@@ -9,3 +9,4 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
